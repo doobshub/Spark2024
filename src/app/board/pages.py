9fefd8bbb5795107e1875e7797a6b93db3a1228f
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-15 14:21:34 -0400

    chore: Catch up

--- src/app/board/pages.py
+++ src/app/board/pages.py
@@ -1,11 +1,25 @@
 from flask import Blueprint, render_template
 from .util import getParkingSpots
+from detect import ParkingSpaceDetector
 
 bp = Blueprint("pages", __name__)
 
 bp.route("/")
+
+
 def home():
     return render_template("pages/home.html")
+
+
+def getParkingSpots():
+    regions_path = "presentationRegion.p"
+    image_path = "presentationImage.png"
+
+    detector = ParkingSpaceDetector(regions_path)
+
+    _, empty_count = detector.process_image_and_highlight(image_path)
+
+    return empty_count
 
 
 @bp.route("/")
